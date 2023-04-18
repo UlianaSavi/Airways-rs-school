a1e88a28bd153c3d9789241d1601700b6276c5ef
Bator B <batorbb@gmail.com>
2023-04-18 21:34:21 +0200

    fix: fix after merge

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -4,9 +4,17 @@ import { SearchRoutingModule } from './search-routing.module';
 import { ResultListComponent } from './components/result-list/result-list.component';
 import { SearchFormComponent } from './components/search-form/search-form.component';
 import { DetailsComponent } from './components/details/details.component';
+import { AdditionPassengersComponent } from './components/addition-passengers/addition-passengers.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [ResultListComponent, SearchFormComponent, DetailsComponent],
-  imports: [CommonModule, SearchRoutingModule],
+  declarations: [
+    ResultListComponent,
+    SearchFormComponent,
+    DetailsComponent,
+    AdditionPassengersComponent,
+  ],
+  imports: [CommonModule, SearchRoutingModule, ReactiveFormsModule, SharedModule],
 })
 export class SearchModule {}
