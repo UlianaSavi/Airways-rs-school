8baaa56c033a75a40119cdff6dbc22d889819e3d
UlianaSavi <uljasha.sav@gmail.com>
2023-04-20 17:03:23 +0600

    refactor: delete trash

--- src/app/cart/components/cart-form/cart-form.component.ts
+++ src/app/cart/components/cart-form/cart-form.component.ts
@@ -1,35 +1,8 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-cart-form',
   templateUrl: './cart-form.component.html',
   styleUrls: ['./cart-form.component.scss'],
 })
-export class CartFormComponent {
-  constructor(private autServise: AuthService) {}
-
-  testRegister = () => {
-    const test = {
-      name: {
-        firstName: 'tester4',
-        lastName: 'tester4',
-      },
-      email: 'tester4@test.ru',
-      password: '666666',
-      birthDate: '08.12.1999',
-      gender: '',
-      phone: '',
-      citizenship: '',
-    };
-
-    this.autServise.register(test);
-  };
-
-  testLogIn = () => {
-    const testEmail = 'tester4@test.ru';
-    const testPassword = '666666';
-
-    this.autServise.login(testEmail, testPassword);
-  };
-}
+export class CartFormComponent {}
