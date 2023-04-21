3f6bc09ea8222c26c5ba7e6de72f228ebeb7792e
Darkonic10 <tiynov2@mail.ru>
2023-04-21 18:22:42 +0500

    feat: Add validation for sing in forms, minor changes in styles

--- src/app/profile/sing-in-form/sing-in-form.component.ts
+++ src/app/profile/sing-in-form/sing-in-form.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SingInStatusService } from '../../core/services/sing-in-status.service';
 import { Subscription } from 'rxjs';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-sing-in-form',
@@ -8,6 +16,44 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./sing-in-form.component.scss'],
 })
 export class SingInFormComponent implements OnInit, OnDestroy {
+  passwordPattern = '(?=.*[0-9])(?=.*[!@#$%^?&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^?&*]{8,}';
+
+  logInForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  registrationForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern(this.passwordPattern)]),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    dateOfBird: new FormControl('', [Validators.required, this.dateOfBirdValidator()]),
+    gender: new FormControl('', [Validators.required]),
+    countryCode: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    citizenship: new FormControl('', [Validators.required]),
+    agree: new FormControl('', [Validators.required, Validators.requiredTrue]),
+  });
+
+  dateOfBirdValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const adultDate = new Date();
+      adultDate.setFullYear(adultDate.getFullYear() - 18);
+      const invalidDate = new Date(control.value) > adultDate;
+      return invalidDate ? { validDate: { value: control.value } } : null;
+    };
+  }
+
+  // Методы с данными форм для дальнейшего использования
+  login() {
+    console.log('Данные формы singIn>', this.logInForm.value);
+  }
+
+  registration() {
+    console.log('Данные формы регистрации>', this.registrationForm.value);
+  }
+
   hide = true;
 
   constructor(private singInStatusService: SingInStatusService) {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MaterialModule } from './material';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, RouterLink, RouterLinkActive],
-  exports: [RouterLink, RouterLinkActive, MaterialModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive, NgOptimizedImage],
+  exports: [RouterLink, RouterLinkActive, MaterialModule, NgOptimizedImage, ReactiveFormsModule],
 })
 export class SharedModule {}
