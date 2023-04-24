733de237dd86955c18105a42b15aa3a029c49d72
UlianaSavi <uljasha.sav@gmail.com>
2023-04-24 14:58:36 +0600

    feat&fix: fix header styles and add steps prev logic

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SingInStatusService } from '../../services/sing-in-status.service';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../../services/auth.service';
+import { CurrentStep } from '../../models/current-step.model';
 
 @Component({
   selector: 'app-header',
@@ -15,6 +16,14 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   singInActive = false;
 
+  stepsActivate = true;
+
+  activeSearchStep = false;
+
+  activeBookingStep = false;
+
+  activeLastStep = false;
+
   changeStatusSingIn() {
     this.singInStatusService.setSingInStatus(!this.singInActive);
   }
@@ -23,6 +32,29 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.singInSubscription = this.singInStatusService.singInStatus$.subscribe(
       (status) => (this.singInActive = status)
     );
+    this.getCurrStep();
+  }
+
+  getCurrStep() {
+    switch (location.pathname) {
+      case CurrentStep.activeSearchStep:
+        this.activeSearchStep = true;
+        break;
+      case CurrentStep.activeBookingStep:
+        this.activeSearchStep = true;
+        this.activeBookingStep = true;
+        break;
+      case CurrentStep.activeLastStep:
+        this.activeSearchStep = true;
+        this.activeBookingStep = true;
+        this.activeLastStep = true;
+        break;
+      default:
+        this.activeSearchStep = false;
+        this.activeBookingStep = false;
+        this.activeLastStep = false;
+        break;
+    }
   }
 
   ngOnDestroy(): void {
