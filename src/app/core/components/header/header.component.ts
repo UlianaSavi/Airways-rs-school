fbc344dac3ffe2cdb0c385ec75cd54b8391d76fd
UlianaSavi <uljasha.sav@gmail.com>
2023-04-24 15:35:57 +0600

    feat: provided one way trip

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, DoCheck } from '@angular/core';
 import { SingInStatusService } from '../../services/sing-in-status.service';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../../services/auth.service';
@@ -9,14 +9,14 @@ import { CurrentStep } from '../../models/current-step.model';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent implements OnInit, OnDestroy, DoCheck {
   constructor(private singInStatusService: SingInStatusService, public authServise: AuthService) {}
 
   singInSubscription: Subscription | undefined;
 
   singInActive = false;
 
-  stepsActivate = true;
+  stepsActivate = false;
 
   activeSearchStep = false;
 
@@ -32,24 +32,31 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.singInSubscription = this.singInStatusService.singInStatus$.subscribe(
       (status) => (this.singInActive = status)
     );
+  }
+
+  ngDoCheck() {
     this.getCurrStep();
   }
 
   getCurrStep() {
     switch (location.pathname) {
       case CurrentStep.activeSearchStep:
+        this.stepsActivate = true;
         this.activeSearchStep = true;
         break;
       case CurrentStep.activeBookingStep:
+        this.stepsActivate = true;
         this.activeSearchStep = true;
         this.activeBookingStep = true;
         break;
       case CurrentStep.activeLastStep:
+        this.stepsActivate = true;
         this.activeSearchStep = true;
         this.activeBookingStep = true;
         this.activeLastStep = true;
         break;
       default:
+        this.stepsActivate = false;
         this.activeSearchStep = false;
         this.activeBookingStep = false;
         this.activeLastStep = false;
