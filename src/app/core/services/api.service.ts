42ca728934387af2fe87dd2537514453f2484998
UlianaSavi <uljasha.sav@gmail.com>
2023-04-26 17:34:08 +0600

    refactor: delete logs from api servise methods

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IQueryParams } from '../models/query-params.model';
+import { ITicket } from 'src/app/search/models/tickets.model';
 
 @Injectable({
   providedIn: 'root',
@@ -12,21 +13,17 @@ export class ApiService {
     const url = '/tickets';
     const params = new HttpParams()
       .set('from', `${query.from}`)
-      .set('to', `${query.destination}`)
-      .set('dateFrom', `${query.dateFrom}`);
-    return this.http.get(url, { params }).subscribe((response) => {
-      console.log(response);
-    });
+      .set('dateFrom', `${query.dateFrom}`)
+      .set('to', `${query.destination}`);
+    return this.http.get<ITicket[]>(url, { params });
   };
 
   getOneWayTickets = (query: IQueryParams) => {
-    const url = `/tickets?counrty.from=${query.from}`;
+    const url = `/tickets?counrty.from=${query.from}.dateFrom=${query.dateFrom}`;
     const params = new HttpParams()
       .set('from', `${query.from}`)
-      .set('to', `${query.destination}`)
-      .set('dateFrom', `${query.dateFrom}`);
-    return this.http.get(url, { params }).subscribe((response) => {
-      console.log(response);
-    });
+      .set('dateFrom', `${query.dateFrom}`)
+      .set('to', `${query.destination}`);
+    return this.http.get<ITicket[]>(url, { params });
   };
 }
