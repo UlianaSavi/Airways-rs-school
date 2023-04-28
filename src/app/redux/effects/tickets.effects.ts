c4835c6190b80145474854496dadfef85346b681
UlianaSavi <uljasha.sav@gmail.com>
2023-04-28 13:05:33 +0600

    refactor&feat: refactor effect using actions and add act for one way

--- src/app/redux/effects/tickets.effects.ts
+++ src/app/redux/effects/tickets.effects.ts
@@ -2,33 +2,33 @@ import { Injectable } from '@angular/core';
 import { ofType, createEffect, Actions } from '@ngrx/effects';
 import { map, switchMap } from 'rxjs';
 import { ApiService } from 'src/app/core/services/api.service';
-import { ApiTicketsType, SetAllTickets } from '../actions/tickets.actions';
+import * as TicketsActions from '../actions/tickets.actions';
 
 @Injectable()
 export class TicketsEffects {
   loadAllTickets$ = createEffect(() => {
     return this.actions$.pipe(
-      ofType(ApiTicketsType),
+      ofType(TicketsActions.ApiTicketsType),
       switchMap(() =>
         this.dataService.getAllTickets().pipe(
           map((tickets) => {
-            return SetAllTickets({ tickets });
+            return TicketsActions.SetAllTickets({ tickets });
           })
         )
       )
     );
   });
 
-  // loadOneWayTickets$ = createEffect(() => {
-  //   return this.actions$.pipe(
-  //     ofType(ApiOneWayTicketsType),
-  //     switchMap((action = ApiOneWayTicketsType.props.query) =>
-  //       this.dataService
-  //         .getOneWayTickets(action.query)
-  //         .pipe(map((tickets) => SetOneWayTickets({ tickets })))
-  //     )
-  //   );
-  // });
+  loadOneWayTickets$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(TicketsActions.ApiOneWayTicketsType),
+      switchMap((action) =>
+        this.dataService
+          .getOneWayTickets(action.query)
+          .pipe(map((tickets) => TicketsActions.SetOneWayTickets({ tickets })))
+      )
+    );
+  });
 
   constructor(private actions$: Actions, private dataService: ApiService) {}
 }
