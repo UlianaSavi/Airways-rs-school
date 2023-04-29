a8e8a22e3a70e25c5875ada02d0329f1b2f8a01a
Uliana Savitskaya <105851981+UlianaSavi@users.noreply.github.com>
2023-04-29 18:00:45 +0200

    Limits and submit on edit (#15)
    * feat: add min and max dates for tickets search
    
    * feat: add getting cities from server instead moked data
    
    * feat: add submit on search edit

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,13 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ITicket } from 'src/app/search/models/tickets.model';
-import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  constructor(private http: HttpClient, private route: ActivatedRoute) {}
+  constructor(private http: HttpClient) {}
 
   getAllTickets = () => {
     const url = '/tickets';
