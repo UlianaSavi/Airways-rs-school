6e8d51a5ca28c6330d51f6d5606ba24c03901927
UlianaSavi <uljasha.sav@gmail.com>
2023-04-29 22:11:22 +0600

    feat: add empty component for contact block

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { BookingRoutingModule } from './booking-routing.module';
 import { PassengersComponent } from './components/passengers/passengers.component';
 import { SummaryComponent } from './components/summary/summary.component';
+import { ContactDetailsBlockComponent } from './components/contact-details-block/contact-details-block.component';
 
 @NgModule({
-  declarations: [PassengersComponent, SummaryComponent],
+  declarations: [PassengersComponent, SummaryComponent, ContactDetailsBlockComponent],
   imports: [CommonModule, BookingRoutingModule],
 })
 export class BookingModule {}
---
+++ src/app/booking/components/contact-details-block/contact-details-block.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-details-block',
+  templateUrl: './contact-details-block.component.html',
+  styleUrls: ['./contact-details-block.component.scss'],
+})
+export class ContactDetailsBlockComponent {}
