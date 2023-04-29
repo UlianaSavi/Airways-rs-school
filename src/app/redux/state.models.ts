2f1370975741e4b9cea8d87faa0fafd722f48911
Bator B <batorbb@gmail.com>
2023-04-29 18:39:37 +0200

    feat: add select ticket state

--- src/app/redux/state.models.ts
+++ src/app/redux/state.models.ts
@@ -6,15 +6,22 @@ import {
   CurrencyDateState,
 } from './reducers/currency-date.reducer';
 import { CatalogState, ticketsFeatureKey, ticketsReducer } from './reducers/tickets.reducer';
+import {
+  SelectTicketsState,
+  selectTicketFeatureKey,
+  selectTicketReducer,
+} from './reducers/select-ticket.reducer';
 
 export interface State {
   [currencyDateFeatureKey]: CurrencyDateState;
   [ticketsFeatureKey]: CatalogState;
+  [selectTicketFeatureKey]: SelectTicketsState;
 }
 
 export const reducers: ActionReducerMap<State> = {
   [currencyDateFeatureKey]: currencyDateReducer,
   [ticketsFeatureKey]: ticketsReducer,
+  [selectTicketFeatureKey]: selectTicketReducer,
 };
 
 export const metaReducers: MetaReducer<State>[] = isDevMode() ? [] : [];
