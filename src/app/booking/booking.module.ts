bfb531906d44b384a02486a8624e71ac31cdbd3c
Dmitriy <110839441+Darkonic10@users.noreply.github.com>
2023-05-01 23:35:02 +0500

    Go to Passengers (#17)
    * feat: update passengers.component, create passengers-info.component and forms with logic and styles
    
    * fix: fix count baggage
    
    * fix: fix according to pr remarks

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -4,9 +4,12 @@ import { CommonModule } from '@angular/common';
 import { BookingRoutingModule } from './booking-routing.module';
 import { PassengersComponent } from './components/passengers/passengers.component';
 import { SummaryComponent } from './components/summary/summary.component';
+import { SharedModule } from '../shared/shared.module';
+import { PassengersInfoComponent } from './components/passengers/passengers-info/passengers-info.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [PassengersComponent, SummaryComponent],
-  imports: [CommonModule, BookingRoutingModule],
+  declarations: [PassengersComponent, SummaryComponent, PassengersInfoComponent],
+  imports: [CommonModule, BookingRoutingModule, SharedModule, FormsModule],
 })
 export class BookingModule {}
