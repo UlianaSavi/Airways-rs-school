3ceabd1737af1fbca6690c6ee09caf1d4cabfd7f
UlianaSavi <uljasha.sav@gmail.com>
2023-05-01 16:34:21 +0600

    feat: add TODO for buttons

--- src/app/booking/components/passengers/passengers.component.ts
+++ src/app/booking/components/passengers/passengers.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-passengers',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class PassengersComponent {
   data = []; // TODO данные с passengers и данные с contacts собираются в один объект здесь
+
+  constructor(private location: Location) {}
+
+  back(): void {
+    // TODO когда будет закончена выдача и выбор билетов, сюда будет поступать инфо о критериях поиска и какие билеты выбраны
+    this.location.back();
+  }
 }
