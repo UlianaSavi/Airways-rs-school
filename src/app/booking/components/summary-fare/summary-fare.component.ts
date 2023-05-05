8ad92291351e5369d03661e65d3dad7480fbc517
Bator B <batorbb@gmail.com>
2023-05-06 01:06:27 +0200

    feat: add data from store in summary-order

--- src/app/booking/components/summary-fare/summary-fare.component.ts
+++ src/app/booking/components/summary-fare/summary-fare.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-summary-fare',
   templateUrl: './summary-fare.component.html',
-  styleUrls: ['./summary-fare.component.scss']
+  styleUrls: ['./summary-fare.component.scss'],
 })
-export class SummaryFareComponent {
-
-}
+export class SummaryFareComponent {}
--- src/app/booking/components/summary-order/summary-order.component.ts
+++ src/app/booking/components/summary-order/summary-order.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, take } from 'rxjs';
+import { PassengerData, PassengerInfo, PassengersType } from 'src/app/core/models/passengers.model';
+import { selectPassengers } from 'src/app/redux/selectors/passengers.selectors';
+import { selectBackTicket, selectTicket } from 'src/app/redux/selectors/select-ticket.selector';
+import { ITicket } from 'src/app/search/models/tickets.model';
 
 @Component({
   selector: 'app-summary-order',
   templateUrl: './summary-order.component.html',
-  styleUrls: ['./summary-order.component.scss']
+  styleUrls: ['./summary-order.component.scss'],
 })
-export class SummaryOrderComponent {
+export class SummaryOrderComponent implements OnInit {
+  @Input() isBack = false;
 
+  ticket$!: Observable<ITicket | null>;
+
+  passengersData$: Observable<Record<PassengersType, PassengerInfo>> =
+    this.store.select(selectPassengers);
+
+  passengers!: Record<PassengersType, PassengerInfo>;
+
+  passArr: PassengersType[] = ['adult', 'child', 'infant'];
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.passengersData$.pipe(take(1)).subscribe((passData) => (this.passengers = passData));
+    this.ticket$ = !this.isBack
+      ? this.store.select(selectTicket)
+      : this.store.select(selectBackTicket);
+  }
+
+  public getAllPassengerInArray() {
+    const pass: ({ passType: PassengersType } & PassengerData)[] = [];
+    Object.keys(this.passengers).forEach((passType) => {
+      this.passengers[passType as PassengersType].data.forEach((data) => {
+        return pass.push({ passType: passType as PassengersType, ...data });
+      });
+    });
+    return pass;
+  }
 }
