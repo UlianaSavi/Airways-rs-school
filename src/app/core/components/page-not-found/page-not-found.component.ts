cbbed974204571f33c1dfd1a58928d8e4489ec9c
Darkonic10 <tiynov2@mail.ru>
2023-05-18 17:37:53 +0500

    feat: add changing favicon on 404-page

--- src/app/core/components/page-not-found/page-not-found.component.ts
+++ src/app/core/components/page-not-found/page-not-found.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss'],
 })
-export class PageNotFoundComponent {}
+export class PageNotFoundComponent implements OnInit, OnDestroy {
+  link = document.querySelector('#appIcon') as HTMLLinkElement;
+
+  previousFavicon = this.link.href;
+
+  ngOnInit(): void {
+    this.link.href = 'assets/favicons/favicon-404.png';
+  }
+
+  ngOnDestroy(): void {
+    this.link.href = this.previousFavicon;
+  }
+}
