1ec8d79e5b6ce4035d52e7dd49e5a6e9a149123a
UlianaSavi <uljasha.sav@gmail.com>
2023-05-24 15:49:49 +0600

    refactor: delete trash

--- src/app/core/services/same-tickets-check.servise.ts
+++ src/app/core/services/same-tickets-check.servise.ts
@@ -1,23 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Store } from '@ngrx/store';
 import { Subject } from 'rxjs';
-import { selectBackTicket, selectTicket } from 'src/app/redux/selectors/select-ticket.selector';
-import { ITicket } from 'src/app/search/models/tickets.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SameTicketsCheckService {
-  constructor(private store: Store) {}
-
-  ticketFrom: ITicket | null = null;
-
-  ticketBack: ITicket | null = null;
-
-  selectTicket$ = this.store.select(selectTicket);
-
-  selectBackTicket$ = this.store.select(selectBackTicket);
-
   private sameTickets$$ = new Subject<boolean>();
 
   sameTickets$ = this.sameTickets$$.asObservable();
